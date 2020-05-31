be05f24f0db184f75745f567ea6dc21b49c1b1b6
James Martin <63636440+Dr-jnm23@users.noreply.github.com>
2020-05-31 16:37:31 +0100

    Update PES draw 2.0.py
    UPDATED FOR LAST 2 GAMES

--- PES draw 2.0.py
+++ PES draw 2.0.py
@@ -1,7 +1,7 @@
 import random
 import datetime
 
-x = ['GER','BRA','ENG','ARG','BEL','ITA','FRA','POR']
+x = ['MAN CITY','JUVENTUS','REAL MADRID','LIVERPOOL','BAYERN','PSG']
 random.shuffle(x)
 
 while x:
@@ -9,15 +9,9 @@ while x:
     Wombat1 = x.pop()
     Womble2 = x.pop()
     Wombat2 = x.pop()
-    Womble3 = x.pop()
-    Wombat3 = x.pop()
-    Womble4 = x.pop()
-    Wombat4 = x.pop()
 
-print((Womble1) + ' vs ' + (Wombat1))
-print((Womble2) + ' vs ' + (Wombat2))
-print((Womble3) + ' vs ' + (Wombat3))
-print((Womble4) + ' vs ' + (Wombat4))
+print((Womble1) + ' vs ' + (Wombat1) + 'WOMBAT HOME')
+print((Womble2) + ' vs ' + (Wombat2) + 'WOMBLE HOME')
 
 today = (datetime.datetime.strftime(datetime.datetime.today() , '%d/%m/%Y-  %Hh:%Mm'))
 print (('this draw was held at ') + (today))
